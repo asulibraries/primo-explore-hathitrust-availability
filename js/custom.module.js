2dd658791786ffbd4298382d0ebf8a94d0148e70
Scotty Carlson <scottythered@users.noreply.github.com>
2020-05-14 14:52:13 -0700

    Updated to v2.6.1 of UMN's release

--- js/custom.module.js
+++ js/custom.module.js
@@ -1,5 +1,5 @@
 angular.module('viewCustom', ['hathiTrustAvailability'])
   .component('prmSearchResultAvailabilityLineAfter', {
-    template: '<hathi-trust-availability></hathi-trust-availability>'
+    template: '<hathi-trust-availability ignore-copyright="false"></hathi-trust-availability>'
   });
 
